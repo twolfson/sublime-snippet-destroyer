c5f9108a05655556c84529d756d37b91e7af425e
Todd Wolfson <todd@twolfson.com>
2013-11-20 02:14:35 -0800

    Added docstrings and up to destroy step

--- snippet-destroyer.py
+++ snippet-destroyer.py
@@ -7,16 +7,34 @@ def noop(*args, **kwargs):
     pass
 
 class SnippetDestroyerDeleteAllCommand(sublime_plugin.ApplicationCommand):
-    def run(self):
-        # Find all of our snippets
+    def get_snippets(self):
+        """Collect all .tmSnippet and .sublime-snippet files in Sublime Text's Packages folder"""
         package_dir = sublime.packages_path()
         tm_snippet_glob = os.path.join(package_dir, '**/*.tmSnippet')
         sublime_snippet_glob = os.path.join(package_dir, '**/*.sublime-snippet')
         snippets = glob.glob(sublime_snippet_glob) + glob.glob(tm_snippet_glob)
+        return snippets
+
+    def run(self):
+        """Destroy every .tmSnippet and .sublime-snippet file in Sublime Text's Packages folder"""
+        # Find all of our snippets
+        snippets = self.get_snippets()
 
         # If there were no snippets found, let the user know
         active_window = sublime.active_window()
         if not snippets:
             active_window.show_quick_panel(['No snippets were found.'], noop)
-
         # Otherwise, prompt the user about actions to take (no as default)
+        else:
+            active_window.show_quick_panel(
+                ['%d snippets were found. Do you want to:' % len(snippets),
+                    'Keep them.', 'Destroy them.'],
+                self.handle_decision)
+
+    def handle_decision(self, index):
+        """Decide to destroy or keep the snippet files"""
+        # If we decided to destroy
+        if index == 2:
+            snippets = self.get_snippets()
+            for filepath in snippets:
+                print filepath
