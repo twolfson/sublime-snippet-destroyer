05b7459b4133f4f52729635866c850c15602c570
Todd Wolfson <todd@twolfson.com>
2013-11-20 02:03:26 -0800

    Finding all snippets

--- snippet-destroyer.py
+++ snippet-destroyer.py
@@ -1,7 +1,17 @@
+import glob
+import os
 import sublime
 import sublime_plugin
 
 
 class SnippetDestroyerDeleteAllCommand(sublime_plugin.ApplicationCommand):
     def run(self):
-        print 'hai'
+        # Find all of our snippets
+        package_dir = sublime.packages_path()
+        tm_snippet_glob = os.path.join(package_dir, '**/*.tmSnippet')
+        sublime_snippet_glob = os.path.join(package_dir, '**/*.sublime-snippet')
+        snippets = glob.glob(sublime_snippet_glob) + glob.glob(tm_snippet_glob)
+
+        # If there were no snippets found, let the user know
+
+        # Otherwise, prompt the user about actions to take (no as default)
