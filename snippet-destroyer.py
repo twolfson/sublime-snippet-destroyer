a8544e4bcaf4d1f1b6668190613548133e48c6a6
Todd Wolfson <todd@twolfson.com>
2013-11-20 02:06:09 -0800

    Handled case for no snippets

--- snippet-destroyer.py
+++ snippet-destroyer.py
@@ -3,6 +3,8 @@ import os
 import sublime
 import sublime_plugin
 
+def noop(*args, **kwargs):
+    pass
 
 class SnippetDestroyerDeleteAllCommand(sublime_plugin.ApplicationCommand):
     def run(self):
@@ -13,5 +15,8 @@ class SnippetDestroyerDeleteAllCommand(sublime_plugin.ApplicationCommand):
         snippets = glob.glob(sublime_snippet_glob) + glob.glob(tm_snippet_glob)
 
         # If there were no snippets found, let the user know
+        active_window = sublime.active_window()
+        if not snippets:
+            active_window.show_quick_panel(['No snippets were found.'], noop)
 
         # Otherwise, prompt the user about actions to take (no as default)
